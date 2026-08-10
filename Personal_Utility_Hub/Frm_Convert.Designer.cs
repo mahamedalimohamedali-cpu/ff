74790c38113c9e6d6581e564c2e5e586f27a9d2c
mahamedalimohamedali-cpu <mahamedalimohamedali@gmail.com>
2026-08-11 01:30:09 +0300

    Add Personal_Utility_Hub/Frm_Convert.Designer.cs

---
+++ Personal_Utility_Hub/Frm_Convert.Designer.cs
@@ -0,0 +1,301 @@
+namespace Personal_Utility_Hub
+{
+    partial class Frm_Convert
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Txt_Letter = new System.Windows.Forms.TextBox();
+            this.btn_Lower = new System.Windows.Forms.Button();
+            this.btn_UpperCase = new System.Windows.Forms.Button();
+            this.btn_TitleCase = new System.Windows.Forms.Button();
+            this.btn_Octal = new System.Windows.Forms.Button();
+            this.btn_Bin = new System.Windows.Forms.Button();
+            this.btn_Hex = new System.Windows.Forms.Button();
+            this.Txt_Number = new System.Windows.Forms.TextBox();
+            this.btn_Decode = new System.Windows.Forms.Button();
+            this.btn_Encode = new System.Windows.Forms.Button();
+            this.Txt_Encrypt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Txt_Letter
+            // 
+            this.Txt_Letter.BackColor = System.Drawing.Color.Black;
+            this.Txt_Letter.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txt_Letter.ForeColor = System.Drawing.SystemColors.Window;
+            this.Txt_Letter.Location = new System.Drawing.Point(583, 183);
+            this.Txt_Letter.Multiline = true;
+            this.Txt_Letter.Name = "Txt_Letter";
+            this.Txt_Letter.Size = new System.Drawing.Size(205, 27);
+            this.Txt_Letter.TabIndex = 0;
+            // 
+            // btn_Lower
+            // 
+            this.btn_Lower.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Lower.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Lower.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Lower.ForeColor = System.Drawing.Color.White;
+            this.btn_Lower.Location = new System.Drawing.Point(698, 234);
+            this.btn_Lower.Name = "btn_Lower";
+            this.btn_Lower.Size = new System.Drawing.Size(90, 42);
+            this.btn_Lower.TabIndex = 2;
+            this.btn_Lower.Text = "LowerCase";
+            this.btn_Lower.UseVisualStyleBackColor = false;
+            this.btn_Lower.Click += new System.EventHandler(this.btn_Lower_Click);
+            // 
+            // btn_UpperCase
+            // 
+            this.btn_UpperCase.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_UpperCase.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_UpperCase.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_UpperCase.ForeColor = System.Drawing.Color.White;
+            this.btn_UpperCase.Location = new System.Drawing.Point(583, 234);
+            this.btn_UpperCase.Name = "btn_UpperCase";
+            this.btn_UpperCase.Size = new System.Drawing.Size(90, 42);
+            this.btn_UpperCase.TabIndex = 1;
+            this.btn_UpperCase.Text = "UpperCase";
+            this.btn_UpperCase.UseVisualStyleBackColor = false;
+            this.btn_UpperCase.Click += new System.EventHandler(this.btn_UpperCase_Click);
+            // 
+            // btn_TitleCase
+            // 
+            this.btn_TitleCase.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_TitleCase.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_TitleCase.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_TitleCase.ForeColor = System.Drawing.Color.White;
+            this.btn_TitleCase.Location = new System.Drawing.Point(635, 294);
+            this.btn_TitleCase.Name = "btn_TitleCase";
+            this.btn_TitleCase.Size = new System.Drawing.Size(90, 42);
+            this.btn_TitleCase.TabIndex = 3;
+            this.btn_TitleCase.Text = "TitleCase";
+            this.btn_TitleCase.UseVisualStyleBackColor = false;
+            this.btn_TitleCase.Click += new System.EventHandler(this.btn_TitleCase_Click);
+            // 
+            // btn_Octal
+            // 
+            this.btn_Octal.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Octal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Octal.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Octal.ForeColor = System.Drawing.Color.White;
+            this.btn_Octal.Location = new System.Drawing.Point(73, 305);
+            this.btn_Octal.Name = "btn_Octal";
+            this.btn_Octal.Size = new System.Drawing.Size(90, 42);
+            this.btn_Octal.TabIndex = 11;
+            this.btn_Octal.Text = "Octal";
+            this.btn_Octal.UseVisualStyleBackColor = false;
+            this.btn_Octal.Click += new System.EventHandler(this.btn_Octal_Click);
+            // 
+            // btn_Bin
+            // 
+            this.btn_Bin.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Bin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Bin.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Bin.ForeColor = System.Drawing.Color.White;
+            this.btn_Bin.Location = new System.Drawing.Point(21, 234);
+            this.btn_Bin.Name = "btn_Bin";
+            this.btn_Bin.Size = new System.Drawing.Size(90, 42);
+            this.btn_Bin.TabIndex = 9;
+            this.btn_Bin.Text = "Bin";
+            this.btn_Bin.UseVisualStyleBackColor = false;
+            this.btn_Bin.Click += new System.EventHandler(this.btn_Bin_Click);
+            // 
+            // btn_Hex
+            // 
+            this.btn_Hex.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Hex.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Hex.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Hex.ForeColor = System.Drawing.Color.White;
+            this.btn_Hex.Location = new System.Drawing.Point(136, 234);
+            this.btn_Hex.Name = "btn_Hex";
+            this.btn_Hex.Size = new System.Drawing.Size(90, 42);
+            this.btn_Hex.TabIndex = 10;
+            this.btn_Hex.Text = "Hex";
+            this.btn_Hex.UseVisualStyleBackColor = false;
+            this.btn_Hex.Click += new System.EventHandler(this.btn_Hex_Click);
+            // 
+            // Txt_Number
+            // 
+            this.Txt_Number.BackColor = System.Drawing.Color.Black;
+            this.Txt_Number.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txt_Number.ForeColor = System.Drawing.SystemColors.Window;
+            this.Txt_Number.Location = new System.Drawing.Point(21, 183);
+            this.Txt_Number.Multiline = true;
+            this.Txt_Number.Name = "Txt_Number";
+            this.Txt_Number.Size = new System.Drawing.Size(205, 27);
+            this.Txt_Number.TabIndex = 8;
+            // 
+            // btn_Decode
+            // 
+            this.btn_Decode.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Decode.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Decode.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Decode.ForeColor = System.Drawing.Color.White;
+            this.btn_Decode.Location = new System.Drawing.Point(296, 234);
+            this.btn_Decode.Name = "btn_Decode";
+            this.btn_Decode.Size = new System.Drawing.Size(90, 42);
+            this.btn_Decode.TabIndex = 5;
+            this.btn_Decode.Text = "Decode";
+            this.btn_Decode.UseVisualStyleBackColor = false;
+            this.btn_Decode.Click += new System.EventHandler(this.btn_Decode_Click);
+            // 
+            // btn_Encode
+            // 
+            this.btn_Encode.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Encode.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Encode.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Encode.ForeColor = System.Drawing.Color.White;
+            this.btn_Encode.Location = new System.Drawing.Point(411, 234);
+            this.btn_Encode.Name = "btn_Encode";
+            this.btn_Encode.Size = new System.Drawing.Size(90, 42);
+            this.btn_Encode.TabIndex = 6;
+            this.btn_Encode.Text = "Encode";
+            this.btn_Encode.UseVisualStyleBackColor = false;
+            this.btn_Encode.Click += new System.EventHandler(this.btn_Encode_Click);
+            // 
+            // Txt_Encrypt
+            // 
+            this.Txt_Encrypt.BackColor = System.Drawing.Color.Black;
+            this.Txt_Encrypt.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txt_Encrypt.ForeColor = System.Drawing.SystemColors.Window;
+            this.Txt_Encrypt.Location = new System.Drawing.Point(296, 183);
+            this.Txt_Encrypt.Multiline = true;
+            this.Txt_Encrypt.Name = "Txt_Encrypt";
+            this.Txt_Encrypt.Size = new System.Drawing.Size(205, 27);
+            this.Txt_Encrypt.TabIndex = 4;
+            this.Txt_Encrypt.TextChanged += new System.EventHandler(this.Txt_Encrypt_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Viner Hand ITC", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(321, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 44);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Convert";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Viner Hand ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label4.Location = new System.Drawing.Point(650, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 26);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Letter";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Viner Hand ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Location = new System.Drawing.Point(359, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 26);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Encryption";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Viner Hand ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label3.Location = new System.Drawing.Point(44, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(164, 26);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Number Converter";
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Close.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Close.ForeColor = System.Drawing.Color.White;
+            this.btn_Close.Location = new System.Drawing.Point(260, 349);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(248, 77);
+            this.btn_Close.TabIndex = 21;
+            this.btn_Close.Text = "Close";
+            this.btn_Close.UseVisualStyleBackColor = false;
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // Frm_Convert
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Decode);
+            this.Controls.Add(this.btn_Encode);
+            this.Controls.Add(this.Txt_Encrypt);
+            this.Controls.Add(this.btn_Octal);
+            this.Controls.Add(this.btn_Bin);
+            this.Controls.Add(this.btn_Hex);
+            this.Controls.Add(this.Txt_Number);
+            this.Controls.Add(this.btn_TitleCase);
+            this.Controls.Add(this.btn_UpperCase);
+            this.Controls.Add(this.btn_Lower);
+            this.Controls.Add(this.Txt_Letter);
+            this.Name = "Frm_Convert";
+            this.Text = "Frm_Convert";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Txt_Letter;
+        private System.Windows.Forms.Button btn_Lower;
+        private System.Windows.Forms.Button btn_UpperCase;
+        private System.Windows.Forms.Button btn_TitleCase;
+        private System.Windows.Forms.Button btn_Octal;
+        private System.Windows.Forms.Button btn_Bin;
+        private System.Windows.Forms.Button btn_Hex;
+        private System.Windows.Forms.TextBox Txt_Number;
+        private System.Windows.Forms.Button btn_Decode;
+        private System.Windows.Forms.Button btn_Encode;
+        private System.Windows.Forms.TextBox Txt_Encrypt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_Close;
+    }
+}
